156bf90b3300a19af7d74ee98921daae3c89ab0a
Paul Ly <paul.ly@live.ca>
2017-08-06 01:36:10 -0400

    All the work I have done so far, basic unity setup
    - Android sdk and working fine
    - added basic menu button to navigate for now

---
+++ Assets/Scripts/Play_Button.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Play_Button : MonoBehaviour
+{
+    public void Button_Click()
+    {
+        Debug.Log("CLICKED BOI");
+        SceneManager.LoadScene(1);
+    }
+}
